5934c592265b79ca4663eba783a7f15bf3d46e1f
noisonnoiton <noisonnoiton@gmail.com>
2021-06-24 18:57:35 +0900

    add env mybatis config rest

--- src/main/java/com/skcc/JsProductServiceApplication.java
+++ src/main/java/com/skcc/JsProductServiceApplication.java
@@ -4,7 +4,10 @@ import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -31,12 +34,17 @@ public class JsProductServiceApplication {
 	@Autowired
 	private ApplicationContext applicationContext;
 	
+	@Value("${mybatis.config-location}")
+	String mybatisConfig;
+
+	private static final Logger log = LoggerFactory.getLogger("main");
+	
 	@Bean
 	public SqlSessionFactory sqlSessionFactory() throws Exception{
 		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
 		sessionFactory.setDataSource(dataSource);
 
-		sessionFactory.setConfigLocation(applicationContext.getResource("classpath:mybatis-config.xml"));
+		sessionFactory.setConfigLocation(applicationContext.getResource("classpath:" + mybatisConfig));
 		
 		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
 		
